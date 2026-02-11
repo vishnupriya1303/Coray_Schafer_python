6fd418432dad484d79b4b2bd10353e0bdc5c54d6
vishnupriya1303 <vishnupriya2k12@gmail.com>
2026-02-12 00:20:22 +0530

    int_Float.py

---
+++ int_float.py
@@ -0,0 +1,12 @@
+#int-for whole numbers
+#float - decimal numbers
+num=5
+num2=3092.2
+#type prints the data type 
+print(type(num))
+print(type(num2))
+
+#operators
+# //- integral div, ** -power operator, %-modula operator for division
+
+
