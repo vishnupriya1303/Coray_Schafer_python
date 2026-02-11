fdfb3f61862b17483c816a91ef85bf7a94272dd4
vishnupriya1303 <vishnupriya2k12@gmail.com>
2026-02-11 23:53:18 +0530

    Initial commit Coray Schafer Python playlist

---
+++ strings.py
@@ -0,0 +1,19 @@
+print("Hello Monu\"s World")
+strr='vishnuuu'
+#use \ or "" '' accordingly
+#for multi line string use  ''' '''
+print('''
+      hey
+      Mona''')
+#to print length
+print(len('Monaa hahah'))
+#to print particular char
+print(strr[1]) #0based indexing
+#to print a slice 
+print(strr[0:3]) #lower inclusive upper excluded
+print(strr.upper()) #similarly lower()
+print(strr.count('u')) #count of particular char/str
+print(strr.find('nu')) #prints the index where that substr starts
+
+strr=strr.replace('u','a') #doesnot change inplace--have to return it and store another value
+print(strr)
